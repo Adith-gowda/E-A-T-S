cbcf5908ba8172e30124b82770e13ad6d5e39f5d
Adith sagar gowda <adithgowda06@gmail.com>
2023-11-18 18:38:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
+import Signup from './Components/Signup';
+import UserProfile from './Components/UserProfile'; 
+import AttendanceSummary from './Components/AttendanceSummary';
+import EditProfile from './Components/EditProfile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AttendanceList from './Components/AttendanceList';
+import AdminLogin from './Components/AdminLogin';
+import AdminHome from './Components/AdminHome';
+import AdminProfile from './Components/AdminProfile';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header companyName="TechTrove" />
+              <Login />
+            </>
+          }
+        />
+        <Route path="/Admin" element={<AdminLogin />} />
+        <Route path="/AdminHome/:id" element={<AdminHome />} />
+        <Route path="/AdminProfile/:id" element={<AdminProfile />} />
+        <Route path="/dashboard/:id" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/user-profile/:id" element={<UserProfile />} /> 
+        <Route path="/editProfile/:id" element={<EditProfile />} />
+        <Route path="/attendance-summary/:id" element={<AttendanceSummary/>} />
+        <Route path="/attendance/:id/:month" element={<AttendanceList/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
